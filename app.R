fb5b96e09d1b6eb0bbd67ecf36dc3f792fb281ab
Emma Peterson <emmacooperpeterson@gmail.com>
2021-09-03 21:07:29 -0500

    added dadbot image

--- app.R
+++ app.R
@@ -1,7 +1,10 @@
 library(shiny)
-source("dad_bot.R")
+library(tidyverse)
 library(shinyjs)
 library(bslib)
+library(lubridate)
+
+# rsconnect::deployApp() to deploy to https://emmacooperpeterson.shinyapps.io/dad_texts
 
 
 # ui ----------------------------------------------------------------------
@@ -26,46 +29,39 @@ ui <- fluidPage(
 
       radioButtons(
         "markov_state_size",
-        label="Markov State Size",
+        label="Markov State Size (probabilities can be based on the previous
+               one or two words)",
         choices=c("1", "2"),
         selected="2",
         inline=TRUE
       ),
 
-      sliderInput(
+      radioButtons(
         "max_overlap_ratio",
-        label="Maximum Overlap Ratio",
-        min=0.25,
-        max=1,
-        step=0.05,
-        value=0.75
+        label="Maximum Overlap Ratio (exclude generated texts that overlap an
+               original text by more than this percentage)",
+        choices=c("20%" = 0.2, "40%" = 0.4, "60%" = 0.6, "80%" = 0.8),
+        selected=0.6,
+        inline=TRUE
       ),
 
-      sliderInput(
+      radioButtons(
         "maximum_sentence_length",
-        label="Maximum Sentence Length",
-        min=10,
-        max=100,
-        step=5,
-        value=50
+        label="Maximum Generrated Text Length",
+        choices=c(20, 40, 60, 80, 100),
+        selected=40,
+        inline=TRUE
       ),
 
-      actionButton("build_model", "Build DadBot3000™", class="button")
+      actionButton("build_model", "Build DadBot3000™", class="button"),
+      hidden(actionButton("generate", "Generate DadText™", class="button"))
     ),
 
     br(),
 
     fluidRow(
-      column(
-        12,
-        align="center",
-        hidden(actionButton("generate", "Generate DadText™", class="button"))
-      )
-    ),
-
-    fluidRow(
-      column(6, imageOutput("image")),
-      column(6, textOutput("dad_text"))
+      column(3, imageOutput("image")),
+      column(9, align="left", textOutput("dad_text"))
     )
   )
 )
@@ -74,23 +70,51 @@ ui <- fluidPage(
 # server ------------------------------------------------------------------
 server <- function(input, output, session) {
 
+  is_dads_birthday <- FALSE
+  now <- today(tzone="EST")
+  if (month(now) == 9 & day(now) == 3) is_dads_birthday <- TRUE
+
+  welcome_message <- if_else(
+    is_dads_birthday,
+    "HAPPY BIRTHDAY!!!",
+    "Welcome to dadBot3000™"
+  )
+
+  description <- "dadBot3000™ is a Markov chain text generator. He was trained
+  on a year of texts sent to Emma by her dad. Once you've built the bot to your
+  specifications, he will deliver newly generated texts where each word is based
+  on the probability of that word appearing after the previous word(s) in the
+  training texts."
+
+  showModal(modalDialog(
+    title = welcome_message,
+    description,
+    footer = modalButton("cool beans !", icon=icon("check"))
+  ))
+
+  inputs <- readRDS("data/inputs.RDS")
+  generated_texts <- readRDS("data/generated_texts.RDS")
+
   # object to store reactive values <-  referenced later via `values$...`
   values <- reactiveValues()
 
-  # when any of the sliders are adjusted, hide the current display
-  observeEvent(as.integer(input$markov_state_size)|input$max_overlap_ratio|input$maximum_sentence_length, {
-    hide("image")
-    hide("generate")
-    hide("dad_text")
+  # when any of the inputs are adjusted, hide the current display
+  observeEvent(as.integer(input$markov_state_size)|as.numeric(input$max_overlap_ratio)|as.numeric(input$maximum_sentence_length), {
+    shinyjs::hide("image")
+    shinyjs::hide("generate")
+    shinyjs::hide("dad_text")
   })
 
-
-  dadBotModel <- reactive({
-    model <- create_model(text_data, input$markov_state_size, input$max_overlap_ratio)
-  })
 
   dadBot <- reactive({
-    dad_bot <- generate_texts(dadBotModel(), input$maximum_sentence_length, 100)
+    index <-
+      inputs %>%
+      filter(markov_state_size == as.integer(input$markov_state_size),
+             max_overlap_ratio == input$max_overlap_ratio,
+             maximum_sentence_length == input$maximum_sentence_length) %>%
+      pull(index)
+
+    dad_bot <- generated_texts[[index]]
   })
 
 
@@ -98,9 +122,9 @@ server <- function(input, output, session) {
   observeEvent(input$build_model, {
 
     # if anything has already been rendered, hide it while we re-build the model
-    hide("dad_text")
-    hide("image")
-    hide("generate")
+    shinyjs::hide("dad_text")
+    shinyjs::hide("image")
+    shinyjs::hide("generate")
 
     # simulate a progress bar
     # the model actually builds very quickly
@@ -110,7 +134,7 @@ server <- function(input, output, session) {
     progress$set(message="building dadBot3000™", value=0)
     progress$inc(1/3) # begin the progress bar before building model
 
-    values$generated_texts <- dadBot()
+    values$generated_texts <- sample(dadBot())  # sample randomizes the order
     values$counter <- 1
 
     # simulate the completion of the progress bar once the model is built
@@ -121,14 +145,14 @@ server <- function(input, output, session) {
 
     # render the image
     output$image <- renderImage({
-      filename <- normalizePath(file.path('./images',"img.png"))
+      filename <- normalizePath(file.path('./images',"dadbot.png"))
 
       # Return a list containing the filename
-      list(src = filename, width = "200", height = "200")
+      list(src = filename, width = "300", height = "300")
     }, deleteFile = FALSE)
 
-    show("image")
-    show("generate")
+    shinyjs::show("image")
+    shinyjs::show("generate")
   })
 
 
@@ -137,7 +161,8 @@ server <- function(input, output, session) {
 
     # case where model produced nothing
     if (all(is.na(values$generated_texts))) {
-      output$dad_text <- renderText("dadBot3000™ is broken :( pls rebuild")
+      msg <- "dadBot3000™ does not like this combination of inputs :( pls rebuild"
+      output$dad_text <- renderText(msg)
 
     } else {
       # if we have any generated texts left, print the next one
@@ -147,10 +172,10 @@ server <- function(input, output, session) {
 
      # otherwise, print that we're out of texts
       } else {
-        output$dad_text <- renderText("all out :(")
+        output$dad_text <- renderText("dadBot3000™ has nothing more to say :(")
       }
     }
-    show("dad_text")
+    shinyjs::show("dad_text")
   })
 
 }
